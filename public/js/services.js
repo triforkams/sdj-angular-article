df3df9318fe6b3c68f047cefc7898e8bc286632f
jettro <jettro@gridshore.nl>
2013-08-28 22:53:32 +0200

    cleaned up code to remove code not needed for step 2

--- public/js/services.js
+++ public/js/services.js
@@ -3,45 +3,24 @@
 /* Services */
 var serviceModule = angular.module('myApp.services', []);
 
-// serviceModule.factory('messages',['$http', function(http) {
-// 	function MessagesService(http) {
-		
-// 		this.storeMessage = function(newMessage,callback) {
-// 			var aMessage = {"description":newMessage};
-// 	    	http.post('/api/message', aMessage).
-// 	    		success(function(data) {
-// 	    			callback({"response":"Message is stored","status":"OK"})
-// 	    		});
-// 		};
-
-// 		this.loadMessages = function(callback) {
-// 			http.get('/api/message').
-// 				success(function(data) {
-// 					callback(data);
-// 				});
-// 		}
-// 	}
-
-// 	return new MessagesService(http);
-// }]);
-
-serviceModule.factory('messages',['Message', function(Message) {
-	function MessagesService(Message) {
+serviceModule.factory('messages',['$http', function(http) {
+	function MessagesService(http) {
 		
 		this.storeMessage = function(newMessage,callback) {
-			var aMessage = new Message({"description":newMessage});
-			aMessage.$save();
-			callback({"response":"Message is stored","status":"OK"});
+			var aMessage = {"description":newMessage};
+	    	http.post('/api/message', aMessage).
+	    		success(function(data) {
+	    			callback({"response":"Message is stored","status":"OK"})
+	    		});
 		};
 
 		this.loadMessages = function(callback) {
-			callback(Message.query());
+			http.get('/api/message').
+				success(function(data) {
+					callback(data);
+				});
 		}
 	}
 
-	return new MessagesService(Message);
-}]);
-
-serviceModule.factory('Message',['$resource', function(resource) {
-	return resource('/api/message/:messageId');
+	return new MessagesService(http);
 }]);
