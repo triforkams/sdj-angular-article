74a71941eb3fa882c5f862f6ee99b26555335c06
jettro <jettro@gridshore.nl>
2013-08-28 23:50:58 +0200

    Removed code that is now in a branch

--- public/js/services.js
+++ public/js/services.js
@@ -3,28 +3,6 @@
 /* Services */
 var serviceModule = angular.module('myApp.services', []);
 
-// serviceModule.factory('messages',['$http', function(http) {
-// 	function MessagesService(http) {
-		
-// 		this.storeMessage = function(newMessage,callback) {
-// 			var aMessage = {"description":newMessage};
-// 	    	http.post('/api/message', aMessage).
-// 	    		success(function(data) {
-// 	    			callback({"response":"Message is stored","status":"OK"})
-// 	    		});
-// 		};
-
-// 		this.loadMessages = function(callback) {
-// 			http.get('/api/message').
-// 				success(function(data) {
-// 					callback(data);
-// 				});
-// 		}
-// 	}
-
-// 	return new MessagesService(http);
-// }]);
-
 serviceModule.factory('messages',['Message', function(Message) {
 	function MessagesService(Message) {
 		
